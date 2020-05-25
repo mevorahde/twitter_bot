22b458d5aa3a7e4688344699311c42a2d332896e
David Mevorah <mevorahde@hotmail.com>
2020-05-25 13:46:40 -0700

    Update api_twitter_bot_app.py file after some testing.

--- api_twitter_bot_app.py
+++ api_twitter_bot_app.py
@@ -10,26 +10,24 @@ load_dotenv(verbose=True)
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-auth = tweepy.OAuthHandler(os.getenv("t_key"),os.getenv("t_key_password"))
-auth.set_access_token(os.getenv("t_access_token"),os.getenv("t_access_token_secret"))
+auth = tweepy.OAuthHandler(os.getenv("t_key"), os.getenv("t_key_password"))
+auth.set_access_token(os.getenv("t_access_token"), os.getenv("t_access_token_secret"))
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 user = api.me()
 
-search_terms = ["#100DaysOfCode","#CodeNewbie", "#Python", "#CSharp"]
-number_of_tweets = 500
+search_terms = ["#100DaysOfCode", "#CodeNewbie", "#Python", "#CSharp"]
+number_of_tweets = 25
 search = None
 for item in search_terms:
     search = item
-for tweet in tweepy.Cursor(api.search, search).items(number_of_tweets):
-    try:
-        print("Tweet Liked")
-        tweet.favorite()
-        time.sleep(60)
-    except tweepy.TweepError as e:
-        print(e.reason)
-    except StopIteration:
-        break
-
-
+    for tweet in tweepy.Cursor(api.search, q=search).items(number_of_tweets):
+        try:
+            print("Tweet Liked")
+            tweet.favorite()
+            time.sleep(2)
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
