388e994552b51cb38e7b634184e4dfb4611f4f4d
David Mevorah <mevorahde@hotmail.com>
2020-05-25 02:21:12 -0700

    Add api_twitter_bot_app.py file.

---
+++ api_twitter_bot_app.py
@@ -0,0 +1,35 @@
+import tweepy
+import time
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Activate '.env' file
+load_dotenv()
+load_dotenv(verbose=True)
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+auth = tweepy.OAuthHandler(os.getenv("t_key"),os.getenv("t_key_password"))
+auth.set_access_token(os.getenv("t_access_token"),os.getenv("t_access_token_secret"))
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+search_terms = ["#100DaysOfCode","#CodeNewbie", "#Python", "#CSharp"]
+number_of_tweets = 500
+search = None
+for item in search_terms:
+    search = item
+for tweet in tweepy.Cursor(api.search, search).items(number_of_tweets):
+    try:
+        print("Tweet Liked")
+        tweet.favorite()
+        time.sleep(60)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+
